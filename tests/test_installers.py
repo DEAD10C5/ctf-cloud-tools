3b50f11e4cf350e59717eb8b16d29d33a2a4eebe
FrankTheTank <2730246+devsecfranklin@users.noreply.github.com>
2023-07-01 11:45:09 -0400

    test out testing

--- tests/test_installers.py
+++ tests/test_installers.py
@@ -8,8 +8,8 @@ import pexpect
 class TestInstallers(unittest.TestCase):
 
     def test_install_aws(self):
-        child = pexpect.spawn('docker run -it ghcr.io/devsecfranklin/only-feet-cloud-ctf-tools:latest ash')
-        child.expect('💰 onlyfeet@all-money 💰 ~ $')
+        child = pexpect.spawn('docker run -it ghcr.io/devsecfranklin/only-feet-cloud-ctf-tools:latest ash -l')
+        child.expect('~ $ ')
         child.sendline('bin/install_aws.sh')
         child.expect(pexpect.EOF, timeout=None)
         cmd_show_data = child.before
