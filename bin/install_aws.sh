c31beb9c493e9466ef1e81ab05c7606f570c455f
FrankTheTank <2730246+devsecfranklin@users.noreply.github.com>
2023-07-01 12:22:46 -0400

    work on installer scripts

--- bin/install_aws.sh
+++ bin/install_aws.sh
@@ -15,10 +15,4 @@ py3-pip \
 && pip3 install --no-cache-dir awscli \
 && rm -rf /var/cache/apk/*
 
-# mkdir /home/onlyfeet/awscli
-# cd /home/onlyfeet/awscli && \
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# unzip /home/onlyfeet/awscli/awscliv2.zip
-# /home/onlyfeet/awscli/aws/install
-
 # https://docs.aws.amazon.com/IAM/latest/UserGuide/what-is-access-analyzer.html
--- bin/install_scoutsuite.sh
+++ bin/install_scoutsuite.sh
@@ -1,1 +1,11 @@
+#!/usr/bin/env bash
+#
+# SPDX-FileCopyrightText: 2023 DE:AD:10:C5 <thedevilsvoice@dead10c5.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+set -euo pipefail
+IFS=$'\n\t'
+WORKDIR='/home/onlyfeet/workspace'
+
 https://github.com/nccgroup/ScoutSuite.git
